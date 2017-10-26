9a4ce692be279129aad98c4e2fe0d9091c2106ed
philippbruhin <info@philippbruhin.ch>
2017-10-27 00:31:03 +0200

    added style

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,8 +1,23 @@
-<h1>Latest blog post</h1>
-<hr />
+@extends('layouts.app')
 
-@foreach ($blogs as $blog)
-    <h1>{{ $blog->title }}</h1>
-    <p>{{ $blog->body }}</p>
+@section('content')
 
-@endforeach
+<main class="container">
+
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h1>Latest blog post</h1>
+        </div>
+        <div class="col-sm-12">
+            @foreach ($blogs as $blog)
+                <article>
+                    <h1>{{ $blog->title }}</h1>
+                    <p>{{ $blog->body }}</p>
+                </article>
+            @endforeach
+        </div>
+    </div>
+</main>
+
+
+@endsection
