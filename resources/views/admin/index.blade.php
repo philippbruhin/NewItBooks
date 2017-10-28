32d7dbb9b7522c0346a8e2be4cb2a20d47e76ffa
philippbruhin <info@philippbruhin.ch>
2017-10-28 12:24:53 +0200

    pretify home page

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <main class="container-fluid">
+
+        <div class="container-fluid">
+            <div class="jumbotron">
+                <h1>Admin Dashboard</h1>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2">
+                <button class="btn btn-primary"><a style="color:#fff;" href="{{ url('blog/create') }}">Create Blog</a></button>
+                <button class="btn btn-danger"><a style="color:#fff;" href="{{ url('blog/bin') }}">Trashed Blogs</a></button>
+            </div>
+        </div>
+    </main>
+
+
+@endsection
