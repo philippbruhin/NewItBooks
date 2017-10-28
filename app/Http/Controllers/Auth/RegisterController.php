11bf25d0b3c08e67fa6f8f9e97a97df9b5b5632c
philippbruhin <info@philippbruhin.ch>
2017-10-28 12:07:22 +0200

    added admin controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,6 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'role_id' => 2,
         ]);
     }
 
@@ -67,6 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'role_id' => 2,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::get('/blog/{id}/edit', 'BlogController@edit');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('admin', 'AdminController@index');
+
